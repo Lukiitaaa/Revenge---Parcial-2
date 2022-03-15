ea980bd93e0a3bf0317aa31faa1e363e6876ff95
Luka <94508681+Lukiitaaa@users.noreply.github.com>
2022-03-15 00:27:35 -0300

    Add files via upload

---
+++ Revenge 3.0/funciones.h
@@ -0,0 +1,12 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+void RandomDados(int v[],int cant);
+
+void MostrarDados (int v[],int cant);
+
+void RandomDados12(int v[], int cant);
+
+int Jugador_Actual (bool J1);
+
+#endif // FUNCIONES_H_INCLUDED
---
+++ Revenge 3.0/main.cpp
@@ -0,0 +1,139 @@
+#include "rlutil.h"
+#include <iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include "funciones_revenge.h"
+#include <ctime>
+using namespace std;
+
+
+
+int main()
+{
+   int opcion,juego, est;
+   char salir;
+   bool incorrecto = true;
+   srand(time(NULL));
+   int J1 = 6, J2 = 6, Compartidos = 2, Numero_Objetivo = 0, seleccionar, suma = 0, DadosResta = 0, PRUEBA = 8;
+   int vDados1[J1], vDados2[J2], vDados12[2], stats[10] = {0}, a=0;
+   bool otra = false, J = false;
+   string nombre, n[10];
+   for(int e = 0; e<10;e++){
+    n[e] = "VACIO";
+   }
+
+
+    while(incorrecto == true){
+    incorrecto = false;
+    recuadro(1,1,60,20);
+    recuadro(8,4,46,15);
+    rlutil::locate(23,3);
+    cout << "BIENVENIDOS A REVENGE " << endl;
+    rlutil::locate(11,7);
+    cout<<"1- JUGAR ";
+    rlutil::locate(11,9);
+    cout<<"2- ESTADISTICAS ";
+    rlutil::locate(11,11);
+    cout<<"3- CREDITOS";
+    rlutil::locate(11,13);
+    cout<<"0- SALIR";
+    rlutil::locate(11,15);
+    cout<<"ELIJA UNA OPCION: ";
+    cin>>opcion;
+    rlutil::locate(10,20);
+    system("pause");
+    system("cls");
+
+        switch(opcion) {
+            case 1:
+            juego=EmpezarGame();
+            incorrecto = true;
+            rlutil::locate(2,19);
+            cout << "Ingrese nombre para las estadisticas" << endl;
+            rlutil::locate(2,20);
+            cin >> nombre;
+            stats[a] = juego;
+            n[a] = nombre;
+            a+=1;
+            rlutil::anykey();
+
+            break;
+
+            case 2:
+            recuadro1(1,1,60,20);
+
+            rlutil::locate(25,3);
+              cout << "Estadisticas" << endl;
+              for(int z=0; z<10;z++){
+                rlutil::locate(15,z+5);
+                cout<< n[z] << " " <<stats[z] << endl;
+              }
+              rlutil::anykey();
+              incorrecto = true;
+            break;
+
+
+            case 3:
+              recuadro(1,1,60,20);
+              recuadro(8,4,46,15);
+              rlutil::locate(25,3);
+              cout << "CREDITOS" << endl;;
+              rlutil::locate(17,5);
+              cout << "Equipo: Los Revengers " << endl;
+              rlutil::locate(19,9);
+              cout << "Luka Gallo - 25189" << endl;
+              rlutil::locate(15,10);
+              cout << "Tomas Abraham Legajo - 25687" << endl;
+              rlutil::locate(10,20);
+              system("pause");
+              system("cls");
+              incorrecto=true;
+              break;
+
+
+            case 0:
+               recuadro(1,1,60,20);
+               recuadro(8,4,46,15);
+               rlutil::locate(25,3);
+               cout << "Opcion de salida";
+               rlutil::locate(18,5);
+               cout << "Realmente desea salir?" << endl;
+               rlutil::locate(18,6);
+               cout << "S/N: ";
+               cin >> salir;
+               if(salir=='s'|| salir=='S'){
+               rlutil::locate(18,10);
+               cout << "Gracias por jugar REVENGE!" << endl;
+               rlutil::locate(1,22);
+               return 0;
+
+              }
+              else if(salir=='n'|| salir=='N'){
+               incorrecto=true;
+              }
+                break;
+
+            default:
+
+                recuadro(1,1,60,20);
+                rlutil::locate(15,10);
+                rlutil::setColor(rlutil::BLACK);
+                cout << "Comando incorrecto!" << endl;
+                rlutil::setColor(rlutil::BLACK);
+                rlutil::locate(10,20);
+                system("pause");
+                system("cls");
+                incorrecto=true;
+                break;
+        }
+
+
+    }
+
+
+
+
+
+    return 0;
+}
